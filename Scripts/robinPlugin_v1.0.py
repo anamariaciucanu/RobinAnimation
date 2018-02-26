cd2442826dafcf72a96ee5879aea62621a47ec70
Anamaria Ciucanu <anamaria.ciucanu@gmail.com>
2018-02-26 13:49:18 +0000

    Robin hops
    Got the robin to move up and down in the XY plane in a sine wave
    fashion.

--- Scripts/robinPlugin_v1.0.py
+++ Scripts/robinPlugin_v1.0.py
@@ -6,10 +6,10 @@ if cmds.window("robinWindow", exists = True):
     cmds.deleteUI("robinWindow")
     
 #Create window and imge logo
-robinWindow = cmds.window("robinWindow", t="Robin Animator", w = 300, h = 300, sizeable = False)
+robinWindow = cmds.window("robinWindow", t="Robin Animator", w = 400, h = 300, sizeable = False)
 cmds.columnLayout(adj = True)
 logoPath = cmds.internalVar(upd = True) + "icons/robinAnimator.png"
-cmds.image(w=300, h=100, image = logoPath)
+cmds.image(w = 400, h = 100, image = logoPath)
 
 #Create reset button for robin
 cmds.separator(h=10, style = 'double')
@@ -19,34 +19,41 @@ cmds.button( label='Reset Robin', c = "resetRobin()", h = 50)
 cmds.separator(h=10, style = 'double')
 cmds.text('How many frames would you like the nimation to have?');
 animationFrames = cmds.intFieldGrp( numberOfFields=1, label='Animation Frames')
-keySteps = cmds.intFieldGrp( numberOfFields=1, label='Keyframe Steps')
+cmds.separator(h=10, style = 'double')
+cmds.text('How many frames would you like per hop?');
+hopFrames = cmds.intFieldGrp( numberOfFields=1, label='Hop Frames')
+cmds.separator(h=10, style = 'double')
+cmds.text('How high should the robin hop?');
+hopAmplitude = cmds.intFieldGrp( numberOfFields=1, label='Hop Height')
 
 #Create animate button
 cmds.separator(h=10, style = 'double')
 cmds.button( label='Animate Robin', c = "createRobinAnimation()", h = 50)
-
+   
 cmds.showWindow(robinWindow)
+
 
 def createRobinAnimation():
     robinCtrl = cmds.select( 'RobinCTRL', r=True )
     frames = cmds.intFieldGrp(animationFrames, q = True, v = True)
-    steps = cmds.intFieldGrp(keySteps, q = True, v = True)
-    pi=3.14
+    steps = cmds.intFieldGrp(hopFrames, q = True, v = True)
+    amplitude = cmds.intFieldGrp(hopAmplitude, q = True, v = True)
+    pi=math.pi
     
     for i in range(0, frames[0], steps[0]):
         for j in range(0, steps[0], 1):
             radius = steps[0]/2.0
             teta = j*pi/steps[0]
-            csi = j*2*pi/steps[0]
-            newX = radius * math.cos(teta) * math.sin(csi)
-            newY = radius * math.sin(teta) * math.sin(csi)
-            newZ = radius * math.cos(csi)            
+            newY = radius * amplitude[0] * math.fabs(math.sin(teta)) 
+            newX = radius * math.fabs(math.cos(teta)) 
+            newZ = 0              
+      
             oldX = cmds.getAttr('RobinCTRL.translateX') 
             oldY = cmds.getAttr('RobinCTRL.translateY') 
             oldZ = cmds.getAttr('RobinCTRL.translateZ') 
             
             currentX = oldX - newX 
-            currentY = oldY - newY
+            currentY = newY
             currentZ = oldZ - newZ 
             cmds.setAttr('RobinCTRL.translateX', currentX)
             cmds.setKeyframe( 'RobinCTRL', attribute='translateX', t=i+j )
@@ -56,7 +63,6 @@ def createRobinAnimation():
             cmds.setKeyframe( 'RobinCTRL', attribute='translateZ', t=i+j )    
     
     
-
 #Reset robin's position and delete key frames
 maxFrame = cmds.playbackOptions( max = True, query = True )
 
