95f5e156086d19b1106fd76b7df54db43e9a4996
Anamaria Ciucanu <anamaria.ciucanu@gmail.com>
2018-03-26 23:15:43 +0100

    Buggy but beautiful
    Added shake head, it's a bit buggy, because it only goes to one side.
    Also started a method for creating random animation sequences of random
    time intervals.

--- Scripts/robinPlugin_v1.0.py
+++ Scripts/robinPlugin_v1.0.py
@@ -1,5 +1,6 @@
 import maya.cmds as cmds
 import math
+import random  
 
 #If window exists, delete window
 if cmds.window("robinWindow", exists = True):
@@ -26,26 +27,95 @@ cmds.separator(h=10, style = 'double')
 cmds.text('How high should the robin hop?');
 hopAmplitude = cmds.intFieldGrp( numberOfFields=1, label='Hop Height')
 
+#Get parameters from fields and defaults
+pi=math.pi
+start = 0
+end = 120
+steps = 5
+amplitude = 5
+fieldEndGrabbed = False
+fieldStepsGrabbed = False
+fieldAmplitudeGrabbed = False
+
 #Create animate button
 cmds.separator(h=10, style = 'double')
 cmds.button( label='Move Robin', c = "createRobinMoveDirectionAnimation()", h = 50)
 cmds.button( label='Flap Wings', c = "createFlapWingsAnimation()", h = 50)
-   
+cmds.button( label = 'Shake Head', c = "createShakeHeadAnimation()", h = 50)
+cmds.button( label='Create Sequence', c = "createAnimationSequence()", h = 50)
+
+#Show button   
 cmds.showWindow(robinWindow)
 
+#def getStart():
+#    return start
+    
+def getFieldEnd():
+    localEnd = cmds.intFieldGrp(animationFrames, q = True, v = True)
+    global end
+    end = localEnd[0]
 
+def getEnd():
+    global fieldEndGrabbed
+    if not fieldEndGrabbed:
+        getFieldEnd() 
+        fieldEndGrabbed = True   
+    
+def getFieldSteps():
+    localSteps = cmds.intFieldGrp(hopFrames, q = True, v = True)
+    global steps
+    steps = localSteps[0]
+
+def getSteps():
+    global fieldStepsGrabbed
+    if not fieldStepsGrabbed:
+        getFieldSteps() 
+        fieldStepsGrabbed = True  
+        
+def getFieldAmplitude():
+    localAmplitude = cmds.intFieldGrp(hopAmplitude, q = True, v = True)
+    global amplitude
+    amplitude = localAmplitude[0]
+    
+def getAmplitude():
+    global fieldAmplitudeGrabbed
+    if not fieldAmplitudeGrabbed:
+        getFieldAmplitude()
+        fieldAmplitudeGrabbed = True
+
+def createAnimationSequence():
+    animationTypes = random.sample(xrange(5), 5)
+    animationTimes = random.sample(xrange(50), 5)
+    print animationTypes
+    print animationTimes
+    
+    for a in range(0, 5):
+        global end
+        end = end + animationTimes[a]
+        if animationTypes[a] <= 2:
+            #hopping 
+            print "Here 1"
+            createRobinMoveDirectionAnimation()
+        elif animationTypes[a] > 2:
+            #flapping wings 
+            createFlapWingsAnimation()
+            print "Here 2"
+        global start
+        start = end                
+            
+                
+            
 def createRobinMoveDirectionAnimation():
     robinCtrl = cmds.select( 'RobinCTRL', r=True )
-    frames = cmds.intFieldGrp(animationFrames, q = True, v = True)
-    steps = cmds.intFieldGrp(hopFrames, q = True, v = True)
-    amplitude = cmds.intFieldGrp(hopAmplitude, q = True, v = True)
-    pi=math.pi
+    getEnd()
+    getSteps()
+    getFieldAmplitude()    
     
-    for i in range(0, frames[0], steps[0]):
-        for j in range(0, steps[0], 1):
-            radius = steps[0]/2.0
-            teta = j*pi/steps[0]
-            newY = radius * amplitude[0] * math.fabs(math.sin(teta)) 
+    for i in range(start, end, steps):
+        for j in range(0, steps, 1):
+            radius = steps/2.0
+            teta = j*pi/steps
+            newY = radius * amplitude * math.fabs(math.sin(teta)) 
             newX = radius * math.fabs(math.cos(teta)) 
             newZ = 0              
       
@@ -66,24 +136,42 @@ def createRobinMoveDirectionAnimation():
 
 def createFlapWingsAnimation():
     robinCtrl = cmds.select( 'RobinCTRL', r=True )
-    frames = cmds.intFieldGrp(animationFrames, q = True, v = True)
-    steps = cmds.intFieldGrp(hopFrames, q = True, v = True)
-    amplitude = cmds.intFieldGrp(hopAmplitude, q = True, v = True)
-    pi=math.pi
+    getEnd()
+    getSteps()
+    getFieldAmplitude()   
     
-    for i in range(0, frames[0], steps[0]):
-        for j in range(0, steps[0], 1):
-            radius = steps[0]/2.0
-            teta = j*pi/steps[0]
-            rightWingRotation = radius * amplitude[0] * math.sin(teta) 
-            leftWingRotation = -radius * amplitude[0] * math.sin(teta)
+    for i in range(start, end, steps):
+        for j in range(0, steps, 1):
+            radius = steps/2.0
+            teta = j*pi/steps
+            rightWingRotation = radius * amplitude * math.sin(teta) 
+            leftWingRotation = -radius * amplitude * math.sin(teta)
         
             cmds.setAttr('RobinCTRL.LiftRightWing', rightWingRotation)
             cmds.setKeyframe( 'RobinCTRL', attribute='LiftRightWing', t=i+j )
             cmds.setAttr('RobinCTRL.LiftLeftWing', leftWingRotation)
-            cmds.setKeyframe( 'RobinCTRL', attribute='LiftLeftWing', t=i+j )
+            cmds.setKeyframe('RobinCTRL', attribute='LiftLeftWing', t=i+j )
     
 
+def createShakeHeadAnimation():
+    robinCtrl = cmds.select('RobinCTRL', r=True)
+    getEnd()
+    getSteps()
+    getFieldAmplitude()   
+    
+    for i in range(start, end, steps):
+        for j in range(0, steps, 1):
+            radius = steps/2.0
+            teta = j*pi/steps
+            print teta
+            headRotation = radius * amplitude * math.sin(teta) 
+        
+            if headRotation > 90.0:
+                headRotation = 90.0
+            cmds.setAttr('RobinCTRL.ShakeHead', headRotation)
+            cmds.setKeyframe('RobinCTRL', attribute='ShakeHead', t=i+j )
+
+    
     
 #Reset robin's position and delete key frames
 maxFrame = cmds.playbackOptions( max = True, query = True )
